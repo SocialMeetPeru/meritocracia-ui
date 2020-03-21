b9bd3b84eea14a85994374f8cb62b033e15f14ff
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2020-03-21 15:40:08 -0500

    [MR-12] added api service and configured styles

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/theme.scss'
+import ApiService from './services/api'
 
 Vue.config.productionTip = false
+ApiService.init()
 
 new Vue({
   router,
